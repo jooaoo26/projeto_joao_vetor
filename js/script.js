14e9ff6b1181be4f312b0a7101d555690a5b96b6
jooaoo26 <161589090+jooaoo26@users.noreply.github.com>
2024-08-20 08:42:32 -0300

    Add files via upload
    tabela vetor que conta numeros iguais 2

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
-bbbbbbb
+let vetor = [];
+let tbodyValores = document.getElementById('tbody-valores');
+let resultadoContagem = document.getElementById('resultado-contagem');
+
+function adicionarValor() {
+  const valor = Number(document.getElementById('valor').value);
+  vetor.push(valor);
+
+  const tabela = document.getElementById('tabelaValores').getElementsByTagName('tbody')[0];
+  const novalinha = tabela.insertRow();
+  const celula = novalinha.insertCell();
+  celula.textContent = valor;
+
+  document.getElementById('valor').value = '';
+}
+
+function contarDois() {
+  let contador = 0;
+  for (let i = 0; i < vetor.length; i++) {
+    if (vetor[i] === 2) {
+      contador++;
+    }
+  }
+  resultadoContagem.textContent = `Existem ${contador} elementos iguais a 2 no vetor.`;
+}
